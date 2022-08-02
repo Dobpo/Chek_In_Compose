5cfea6ed01cedd1ab617d8b50ad38a6625be0c21
AlexKr <duden89@gmail.com>
2022-08-02 12:21:15 +0200

    Added dark mode

--- app/src/main/java/com/chekincompose/ok/MainActivity.kt
+++ app/src/main/java/com/chekincompose/ok/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.chekincompose.ok
 
+import android.content.res.Configuration
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -14,38 +20,62 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.chekincompose.ok.models.Message
+import com.chekincompose.ok.ui.theme.ChekInComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(Message("Hello ", "Compose"))
+            ChekInComposeTheme {
+                MessageCard(Message("Hello ", "Compose"))
+            }
         }
+    }
+}
+
+@Preview(name = "Light Mode")
+@Preview(
+    name = "Night Mode",
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true
+)
+@Composable
+fun PreviewMessage() {
+    ChekInComposeTheme {
+        MessageCard(Message("Hello ", "Compose"))
     }
 }
 
 @Composable
 fun MessageCard(message: Message) {
-    Row {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
         Image(
             painter = painterResource(id = R.drawable.img_profile),
             contentDescription = "Contact profile image",
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
 
         Spacer(modifier = Modifier.width(8.dp))
 
         Column {
-            Text(message.author)
-            Text(message.body)
+            Text(
+                text = message.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
+
+            Spacer(Modifier.width(4.dp))
+
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                Text(
+                    text = message.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    style = MaterialTheme.typography.body2
+                )
+            }
         }
     }
-}
-
-@Preview
-@Composable
-fun PreviewMessage() {
-    MessageCard(Message("Hello ", "Compose"))
 }
--- app/src/main/java/com/chekincompose/ok/ui/theme/Theme.kt
+++ app/src/main/java/com/chekincompose/ok/ui/theme/Theme.kt
@@ -1,5 +1,6 @@
 package com.chekincompose.ok.ui.theme
 
+import android.util.Log
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
